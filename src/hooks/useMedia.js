ea3cb2d7bc2cd9cbfcec8c33d77f064faa8c69b6
Alex Anderson <raanderson@instructure.com>
2020-06-28 12:45:29 -0600

    Fixes

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react"
 // Hook
 export function useMedia(queries, values, defaultValue) {
   // Array containing a media query list for each query
-  const mediaQueryLists = queries.map(q => window.matchMedia(q))
+  const mediaQueryLists = queries.map(q => {
+    if (typeof window === "undefined") return ""
+
+    return window.matchMedia(q)
+  })
 
   // Function that gets value based on matching media query
   const getValue = () => {
